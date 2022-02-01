523daa6e493f06f945e5740ebc30399fc9ebd4e0
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-01-31 21:42:36 -0300

    controller

---
+++ backend/Felipe.Backend.Locsonging/Felipe.Backend.Locsonging/Controller/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Felipe.Backend.Locsonging.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
