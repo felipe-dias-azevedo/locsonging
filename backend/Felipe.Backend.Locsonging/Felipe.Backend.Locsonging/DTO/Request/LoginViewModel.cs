381c587b5c31f1e6460f83543fb06d0b386c00cf
felipe-dias-azevedo <felipe.azevedo@bandtec.com.br>
2022-02-07 00:05:28 -0300

    changes

--- backend/Felipe.Backend.Locsonging/Felipe.Backend.Locsonging/DTO/Request/LoginViewModel.cs
+++ backend/Felipe.Backend.Locsonging/Felipe.Backend.Locsonging/DTO/Request/LoginViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace Felipe.Backend.Locsonging.DTO.Request
+﻿using Newtonsoft.Json;
+
+namespace Felipe.Backend.Locsonging.DTO.Request
 {
     public class LoginViewModel
     {
+        [JsonProperty("email")]
         public string Email { get; set; }
 
+        [JsonProperty("password")]
         public string Password { get; set; }
     }
 }
